e48cebdb1b0661c0a949f02316d35de3f487b36f
Artur Mukhametgalin <sverx3@gmail.com>
2025-07-31 19:12:51 +0300

    add constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -17,3 +17,7 @@ export const DittoWFRegistryAbi = [
 export const entryPointVersion = "0.7";
 
 export const DittoWFRegistryAddress = "0x7D48195F9b04ef4001B23b012411cb2E20ca86A8" as `0x${string}`;
+
+export const IpfsServiceUrl = "https://ipfs-service.develop.dittonetwork.io";
+
+export const ExecutorAddress = "0xF177179963aA0EDE80Be4396d04d970171CF6a36" as `0x${string}`;
